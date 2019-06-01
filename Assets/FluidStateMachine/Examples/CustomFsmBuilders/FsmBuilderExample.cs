be1618a8b8ba3d393567eab28c594319226e0779
Ash Blue <ash@blueashes.com>
2019-05-31 22:13:04 -0600

    feat(builders): removed class inheritance in favor of c# extensions
    Original builders used an abstract base class that is no longer necessary
    
    BREAKING CHANGE: Current custom builders must be replaced with C# extensions that target the builder
    directly. See README.md section on extension for an example.

--- Assets/FluidStateMachine/Examples/CustomFsmBuilders/FsmBuilderExample.cs
+++ Assets/FluidStateMachine/Examples/CustomFsmBuilders/FsmBuilderExample.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
 
+// @TODO Move these into a state and fsm extension test
 namespace CleverCrow.FluidStateMachine.Examples {
-    public class CustomStateBuilder : StateBuilderBase<CustomStateBuilder> {
-        public CustomStateBuilder MyCustomStateBuilderMethod () {
-            _actions.Add(new ActionEnter((action) => {
+    public static class StateBuilderExtensions {
+        public static StateBuilder MyCustomStateBuilderMethod (this StateBuilder builder) {
+            return builder.AddAction(new ActionEnter((action) => {
                 Debug.Log("Registered");
             }));
-            return this;
         }
     }
-    
-    public class FsmBuilderExample : FsmBuilderBase<FsmBuilderExample, CustomStateBuilder> {
-        public void MyCustomFsmMethod () {
+
+    public static class FsmBuilderExtensions {
+        public static FsmBuilder MyCustomFsmMethod (this FsmBuilder builder) {
             Debug.Log("Custom method");
+            return builder;
         }
     }
 
@@ -22,7 +23,7 @@ namespace CleverCrow.FluidStateMachine.Examples {
         }
         
         public void Init () {
-            var fsmBuilder = new FsmBuilderExample()
+            var fsmBuilder = new FsmBuilder()
                 .State(StateId.A, (state) => {
                     state
                         .MyCustomStateBuilderMethod()
